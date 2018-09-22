94a2a6816feded2c672278ce6793f6b63bc96144
Aditia Prasetio <aditia.prasetio12@gmail.com>
2018-09-22 19:22:36 +0700

    revisi signout

--- assets/js/index.js
+++ assets/js/index.js
@@ -26,10 +26,18 @@ function enkripsi(text){
     // return hash;
 }
 
-// signout
-function signout(){
-    // remove data user
-    localStorage.removeItem(USER_DATA);
-    // redirect to homepage (login page)
-    window.location.replace('index.html');
+function signOut() {
+	var auth2 = gapi.auth2.getAuthInstance();
+
+	auth2.signOut().then(function () {
+
+		console.log('User signed out.');
+
+	});
+	
+	/* redirect to homepage (login page) */
+	window.location.replace('index.html');
+	
+	/* remove data user */
+	localStorage.removeItem(USER_DATA);
 }
